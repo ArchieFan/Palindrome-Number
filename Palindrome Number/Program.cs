a116e077f260f0dc2d069962a2e259eb713a018a
Archie Fan <archiefan@gmail.com>
2022-06-30 08:34:28 +0100

    Add project files.

---
+++ Palindrome Number/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        List<char> chars = new List<char>();
+        foreach (char c in x.ToString())
+        {
+            chars.Add(c);
+        }
+        chars.Reverse();
+        if (string.Concat(chars).Equals(x.ToString()))
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+    static void Main()
+    {
+        int x = -121;
+        Solution s = new Solution();
+        Console.WriteLine(s.IsPalindrome(x));
+    }
+}
+
+
