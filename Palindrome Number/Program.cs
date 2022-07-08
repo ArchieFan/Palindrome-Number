ef0b1c0dda9b214bc3694583be733cd304a0cd69
Archie Fan <archiefan@gmail.com>
2022-07-08 16:59:26 +0100

    Added the array clone/copy and compare sample

--- Palindrome Number/Program.cs
+++ Palindrome Number/Program.cs
@@ -1,4 +1,6 @@
-﻿public class Solution
+﻿using System.Linq;
+
+public class Solution
 {
     public bool IsPalindrome(int x)
     {
@@ -15,12 +17,23 @@
         return false;
     }
 
-
-    static void Main()
+    public bool IsPalindrome2(int x)
     {
-        int x = -121;
+        char[] arr1 = x.ToString().ToCharArray();
+        char[] arr2 = (char[])arr1.Clone(); // Clone arr1 to arr2
+        //char[] arr2 = new char[arr1.Length];
+        //arr1.CopyTo(arr2, 0); //copy arr1 to arr2 from index 0
+        Array.Reverse(arr2);
+        return arr1.SequenceEqual(arr2);    // cannot use "==" 
+    }
+
+        static void Main()
+    {
+        int x = 121;
         Solution s = new Solution();
         Console.WriteLine(s.IsPalindrome(x));
+
+        Console.WriteLine(s.IsPalindrome2(x));
     }
 }
 
